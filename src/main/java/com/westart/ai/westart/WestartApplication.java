45c4e60010f00028d3644c359c25f0a667eb67e2
liyuhui <349598982@qq.com>
2026-07-16 10:37:21 +0800

    测试

--- src/main/java/com/westart/ai/westart/WestartApplication.java
+++ src/main/java/com/westart/ai/westart/WestartApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WestartApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WestartApplication.class, args);
+        System.out.println("测试");
+        System.out.println( "111111");
     }
 
 }
