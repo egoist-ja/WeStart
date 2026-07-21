80730f6017da2ce689dacd85b1f10f2263bbf6f5
iself_24 <2166766986@qq.com>
2026-07-21 16:42:08 +0800

    测试分支提交

--- src/main/java/com/westart/ai/westart/WestartApplication.java
+++ src/main/java/com/westart/ai/westart/WestartApplication.java
@@ -10,6 +10,7 @@ public class WestartApplication {
 
         SpringApplication.run(WestartApplication.class, args);
         System.out.println("Westart Application Started");
+        System.out.println("zxy test");
     }
 
 }
