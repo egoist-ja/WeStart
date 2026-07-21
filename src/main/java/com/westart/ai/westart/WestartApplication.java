701db07b76b11c21f456da243c9db36ad59d7570
zwc <2820305219@qq.com>
2026-07-21 16:59:51 +0800

    测试分支提交

--- src/main/java/com/westart/ai/westart/WestartApplication.java
+++ src/main/java/com/westart/ai/westart/WestartApplication.java
@@ -11,7 +11,7 @@ public class WestartApplication {
         SpringApplication.run(WestartApplication.class, args);
         System.out.println("Westart Application Started");
         System.out.println("zxy test");
-        System.out.println("zwc.test");
+        System.out.println("zwc1.test");
     }
 
 }
