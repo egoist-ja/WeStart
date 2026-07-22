5db4c2e1554c73634070cd0fceec9e661f7a73ac
zwc <2820305219@qq.com>
2026-07-22 09:03:23 +0800

    测试分支

--- src/main/java/com/westart/ai/westart/WestartApplication.java
+++ src/main/java/com/westart/ai/westart/WestartApplication.java
@@ -11,6 +11,7 @@ public class WestartApplication {
         SpringApplication.run(WestartApplication.class, args);
         System.out.println("Westart Application Started");
         System.out.println("ckw test");
+        System.out.println("zwc.test");
     }
 
 }
