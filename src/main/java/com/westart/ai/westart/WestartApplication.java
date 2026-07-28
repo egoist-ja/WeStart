e38dee5f35efc9db36e07bcb1febe0bdd9592f8b
zwc <2820305219@qq.com>
2026-07-28 17:38:40 +0800

    测试

--- src/main/java/com/westart/ai/westart/WestartApplication.java
+++ src/main/java/com/westart/ai/westart/WestartApplication.java
@@ -8,14 +8,13 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @SpringBootConfiguration
-@EnableAutoConfiguration
-@ComponentScan
 public class WestartApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(WestartApplication.class, args);
         System.out.println("Westart Application Started");
+        System.out.println("test");
     }
 
 }
